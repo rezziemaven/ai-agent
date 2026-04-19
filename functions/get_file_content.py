a980baab8948351274b180f671a67bc23bfee284
Sherezz Grant <rezziemaven@gmail.com>
2026-04-19 12:08:30 -0400

    Add get_file_content This function returns the content of a file if the file exists and is within the working directory.

---
+++ functions/get_file_content.py
@@ -0,0 +1,26 @@
+import os
+
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        working_dir_path = os.path.abspath(working_directory)
+        full_file_path = os.path.normpath(os.path.join(working_dir_path, file_path))
+        if not os.path.isfile(full_file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        valid_target_dir = (
+            os.path.commonpath([working_dir_path, full_file_path]) == working_dir_path
+        )
+        if not valid_target_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        f = open(full_file_path, "r")
+        content = f.read(MAX_CHARS)
+        if f.read(1):
+            content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return content
+
+    except Exception as e:
+        return f"Error: {e}"

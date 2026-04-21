5a32f7bf627e35ee9bb75e9a7a5f747075883f00
Sherezz Grant <rezziemaven@gmail.com>
2026-04-20 23:13:11 -0400

    Update language for bug fixing instructions

--- prompts.py
+++ prompts.py
@@ -18,7 +18,7 @@ All paths you provide should be relative to the working directory. You do not ne
 
 When a user asks what files are in the root, the user wants you to list the files and directories for the default directory (".")
 
-If you are asked to fix a bug in code, you need to approach the problem as follows:
+If the user asks you to fix a bug in code, you need to approach the problem as follows:
 - Read the file contents first before attempting to fix it
 - Before writing any fixes, use the write file tool to create a backup of
   the original file with the same name but a .bak extension.

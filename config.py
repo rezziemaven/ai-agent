26e3ef78509bdfc78c6b4e4897c65b745cbba313
Sherezz Grant <rezziemaven@gmail.com>
2026-04-20 23:09:33 -0400

    Update config This commit centralises the commonly modified variables to the config file. It allows you to easily change the models for Ollama or Gemini, change the system prompt to be used, and set the working directory all in one place.

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-MAX_CHARS = 10000
+from prompts import *
+
+# LLMS
+GEMINI_MODEL = "gemini-2.5-flash"
+OLLAMA_BASE_MODEL = "minimax-m2.5:cloud"
+
+# Prompts
+SYSTEM_PROMPT = system_prompt
+
+# Directories
+WORKING_DIRECTORY = "./demo"
+
+# Miscellaneous
+MAX_CHARS = 10000 # Max characters to truncate output to when reading a file
+
